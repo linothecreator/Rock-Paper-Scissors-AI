9993e32de4ab432bf5ec8b840c300ba0a5ccef71
Rodney Brown <85382370+linothecreator@users.noreply.github.com>
2023-03-02 16:41:30 -0800

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,8 +1,8 @@
 # import matplotlib.pyplot as plt
 # from mpl_toolkits.mplot3d import Axes3D
 # from sklearn.preprocessing import LabelEncoder
-# from sklearn.model_selection import train_test_split
-# from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 # import pandas as pd
 # from joblib import dump
 # from joblib import load
@@ -11,8 +11,8 @@ import random
 import numpy as np
 
 def player(prev_opponent_play, wins=[], losses=[], ties=[], iteration=[], opponent_history=[], player_history=[], train=True):
-    num=100
-    _span=99
+    num=500
+    _span=400
     if prev_opponent_play == '':
         prev_opponent_play='P'
       
@@ -51,25 +51,27 @@ def player(prev_opponent_play, wins=[], losses=[], ties=[], iteration=[], oppone
     getresults(player_history, opponent_history)
     iteration.append(len(player_history))
 
-    if len(opponent_history)>=num:
-      opponent_history=opponent_history[:-_span]
-      player_history=player_history[:-_span]
-      wins=wins[:-_span]
+    if len(opponent_history)>=num:      
+      opponent_history=opponent_history[-_span:-1]
+      player_history=player_history[-_span:-1]
+      wins=wins[-_span:-1]
       move_to_num = {'R': 0, 'P': 1, 'S': 2}
       player_history = [move_to_num[move] for move in player_history]
+      opponent_history = [move_to_num[move] for move in opponent_history]
       y = opponent_history
-      # print(y)
-      y = [move_to_num[move] for move in y]
-      y = np.array(y).reshape(1, -1)   
-      y = y[-1]
+      y = np.array(y).reshape(1, -1)  
+      y = np.array([opponent_history]).T
       X = np.array([wins, player_history]).T
       # print(X)
+      X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
       model = DecisionTreeClassifier()
-      model.fit(X, y)
-      prediction = model.predict(X)
+      model.fit(X_train, y_train)
+      y_pred = model.predict(X_test)
+      accuracy = accuracy_score(y_test, y_pred)
+      print(accuracy)
       num_to_move = {0: 'R', 1: 'P',  2: 'S'}
-      prediction = [num_to_move[move] for move in prediction]
-      print(prediction[-1])
+      prediction = [num_to_move[move] for move in y_pred]
+      # print(prediction[-1])
       guess = prediction[-1]
       
       
